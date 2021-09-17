843ed73f959a2808c1c886ae4e4bddf6735bdacb
zlatansmail <zlatan.s@outlook.com>
2021-09-17 16:35:59 +0200

    ex11

---
+++ ex11/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    //Only change code below this line
+    var lastName = "Tesla";
+    var thirdLetterOfLastName = [2];
+    firstLetterOfLastName = lastName[0];
+
+    //Only change code above this line
+    return thirdLetterOfLastName;
+}
+
+console.log(main());
+module.exports = main;
