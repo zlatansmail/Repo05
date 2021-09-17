3c2d8d45137a9c06997335ecaf58b7fb478921f6
zlatansmail <zlatan.s@outlook.com>
2021-09-17 15:11:47 +0200

    ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    //Only change code below this line 
+    var myString = "This is my first string. " + "This is my second string.";
+
+    //Only change code above this line
+
+    return myString;
+}
+
+console.log(main());
+module.exports = main;
